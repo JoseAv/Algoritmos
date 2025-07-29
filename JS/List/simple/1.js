c9f78ddf557b9ac27f9b64188fa14f96bfa2c7c9
JoseAv <jm.aranav@gmail.com>
2025-07-29 14:37:13 -0600

    Agregado lista inversa con recursividad y con ciclo normal

--- JS/List/simple/1.js
+++ JS/List/simple/1.js
@@ -23,19 +23,6 @@ class Lista {
         current.next = new Nodo(value)
     }
 
-
-    ViewNodo() {
-        let current = this.head
-        if (!current)
-            return 'No hay datos'
-
-        while (current !== null) {
-            console.log(current.value)
-            current = current.next
-        }
-    }
-
-
     SearchNodo(value) {
         let current = this.head
         if (!current) {
@@ -54,26 +41,73 @@ class Lista {
 
     DeleteNodo(value) {
         let current = this.head
-        if (!current)
-            return 'no hay datos'
 
         if (current.value === value && current.next === null) {
             this.head = null
+            return
         }
 
-        if (current.value === value && current.next !== null) {
-            this.head = current.next
-        }
+        let back = null
+        while (current !== null) {
 
-        while (current.next !== null) {
-            if (current.next.value === value) {
-                current.next = current.next.next
+            if (current.value === value && current.value === this.head.value) {
+                this.head = this.head.next
                 return
             }
+
+            if (current.value === value) {
+                back.next = back.next.next
+                return
+            }
+            back = current
             current = current.next
         }
-
     }
+
+
+    ReverseNodos() {
+        if (!this.head) {
+            return
+        }
+
+        let current = this.head
+        let atras = null
+
+        while (current !== null) {
+            let temp = current.next
+            current.next = atras
+
+            // movimiento
+            atras = current
+            current = temp
+
+
+        }
+    }
+
+    ReverseNodos(current = this.head, atras = null) {
+        if (current === null) {
+            this.head = atras
+            return
+        }
+
+        this.ReverseNodos(current.next, current)
+        current.next = atras
+    }
+
+
+    ViewNodo() {
+        let current = this.head
+        if (!current)
+            return 'No hay datos'
+
+        while (current !== null) {
+            console.log(current.value)
+            current = current.next
+        }
+    }
+
+
 }
 
 
@@ -82,6 +116,5 @@ SimpleList.AddNodo(1)
 SimpleList.AddNodo(2)
 SimpleList.AddNodo(3)
 SimpleList.AddNodo(4)
-SimpleList.DeleteNodo(4)
-console.log(SimpleList.SearchNodo(1))
+SimpleList.ReverseNodos()
 SimpleList.ViewNodo()

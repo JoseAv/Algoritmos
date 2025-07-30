c2e34e8652752f13d33dbde6c736255e56621419
JoseAv <jm.aranav@gmail.com>
2025-07-30 17:02:41 -0600

    Terminado Lista doble enlazada

--- JS/List/ListaDoble/List.js
+++ JS/List/ListaDoble/List.js
@@ -1,7 +1,8 @@
 
+
 export class Nodo {
 
-    constructor(value) {
+    constructor(value, position) {
         this.back = null
         this.next = null
         this.value = value
@@ -29,19 +30,73 @@ class DoubleList {
         }
     }
 
+    AddNodoInAnyOne(value) {
+        if (!this.head)
+            return 'No hay datos'
+
+        let current = this.head
+        let count = 1
+        while (current !== null) {
+            if (count === value) {
+                let temp = current.next
+                current.next = new Nodo(value)
+                current.next.back = current
+                current.next.next = temp
+                temp.back = current.next
+                return
+            }
+            count++
+            current = current.next
+        }
+
+    }
+
+
+    DeleteAll(value) {
+        if (!this.head)
+            return 'No hay datos'
+
+        if (this.head.value === value) {
+            this.head = this.head.next
+            return this.DeleteAll(value)
+        }
+
+
+        let current = this.head
+        while (current !== null) {
+            if (current.value === value) {
+                let temp = current.back
+                console.log(temp.value)
+                console.log(current.value)
+
+                if (temp.next.next) {
+                    temp.next = temp.next.next
+                    temp.next.back = temp
+                } else {
+                    temp.next = null
+                }
+            }
+            current = current.next
+        }
+
+    }
+
+
+
     ViewNodo() {
         if (!this.head)
             return 'No hay datos'
 
         let current = this.head
-        while (current.next !== null) {
+        while (current !== null) {
+            console.log('No', current.value)
             current = current.next
         }
 
-        while (current !== null) {
-            console.log(current.value)
-            current = current.back
-        }
+        // while (current !== null) {
+        //     console.log(current.value)
+        //     current = current.back
+        // }
 
     }
 
@@ -52,6 +107,19 @@ const NewList = new DoubleList()
 NewList.AddNodo(1)
 NewList.AddNodo(2)
 NewList.AddNodo(3)
+NewList.AddNodo(4)
+NewList.AddNodo(5)
+NewList.AddNodo(6)
+NewList.AddNodoInAnyOne(4)
+NewList.AddNodoInAnyOne(4)
+
+NewList.AddNodoInAnyOne(4)
+
+
+
+
+NewList.DeleteAll(4)
+
 
 NewList.ViewNodo()
 

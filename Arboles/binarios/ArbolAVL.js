77b29017b92778c32e988bc748889c55b9fee37b
JoseAv <jm.aranav@gmail.com>
2025-09-02 12:58:36 -0600

    Agregado Mejoras en recorrer nodos en preorden, calculados altura y sabemos la altura, tambien actualizamos como se incertan los nodos usando las nuevas funciones para modificar todo

--- Arboles/binarios/ArbolAVL.js
+++ Arboles/binarios/ArbolAVL.js
@@ -24,43 +24,57 @@ class arbol {
 
         let temp = this.root
         return this.runAndAddNewNodo(value, temp)
-
     }
 
     runAndAddNewNodo(value, temp) {
         if (value === temp.value)
             return
 
-        console.log(value)
-        console.log(temp.value)
         if (value > temp.value && !temp.rigth) {
-            return temp.rigth = new this.nodo(value)
+            temp.rigth = new this.nodo(value)
+            temp.rigth.parents = temp
+            temp.rigth.height = this.caculatehigth(temp.rigth)
+            return
         }
 
         if (value < temp.value && !temp.left) {
-            return temp.left = new this.nodo(value)
-        }
-        return value > temp.value ? this.runAndAddNewNodo(value, temp.rigth) : this.runAndAddNewNodo(value, temp.left)
-    }
-
-    runNodos() {
-        console.log("Raiz:", this.root.value)
-        let tempRigth = this.root.rigth
-        while (tempRigth !== null) {
-            console.log("Rigth:", tempRigth.value)
-            console.log(tempRigth)
-            tempRigth = tempRigth.rigth
+            temp.left = new this.nodo(value)
+            temp.left.parents = temp
+            temp.left.height = this.caculatehigth(temp.left)
+            return
         }
 
-        let tempLeft = this.root.left
-        while (tempLeft !== null) {
-            console.log("Left:", tempLeft.value)
-            tempLeft = tempLeft.left
-        }
+        if (value > temp.value) {
+            this.runAndAddNewNodo(value, temp.rigth)
+            temp.height = this.caculatehigth(temp)
+            return
 
+        } else {
+            this.runAndAddNewNodo(value, temp.left)
+            temp.height = this.caculatehigth(temp)
+            return
+        }
 
     }
 
+    knowHigth(nodo) {
+        return nodo ? nodo.height : 0
+    }
+
+    caculatehigth(nodo) {
+        return 1 + Math.max(this.knowHigth(nodo.rigth), this.knowHigth(nodo.left))
+    }
+
+
+
+    runNodos(nodo = this.root) {
+        if (!nodo)
+            return
+
+        this.runNodos(nodo.left)
+        console.log(`Value: ${nodo.value}, ALtura: ${nodo.height}`)
+        this.runNodos(nodo.rigth)
+    }
 
 }
 
@@ -70,6 +84,10 @@ ArbolAvl.addNodo(5)
 ArbolAvl.addNodo(6)
 ArbolAvl.addNodo(3)
 ArbolAvl.addNodo(4)
+ArbolAvl.addNodo(7)
+ArbolAvl.addNodo(6.6)
+
+
 
 
 

0eccc523a38d947ef2cdc7137b92981bb653f830
JoseAv <jm.aranav@gmail.com>
2025-09-03 23:05:51 -0600

    Agregado Eliminaciones

---
+++ DB/Tablas/DML/Deletes.sql
@@ -0,0 +1,75 @@
+-- DATA DELECTION
+    -- Basic Delete
+
+-- 1
+select * from estudiantes where promedio < 60;
+delete from estudiantes where promedio<60;
+
+--2
+select curso_codigo,fecha_inscripcion from inscripciones where curso_codigo=201;
+
+select curso_codigo, count(*) as totalIncripciones from inscripciones
+group by curso_codigo
+having count(*) < 2;
+
+
+BEGIN;
+delete from inscripciones where curso_codigo in (
+select curso_codigo from inscripciones
+group by curso_codigo
+having count(*) < 2
+    );
+
+delete from cursos where codigo not in (
+select distinct curso_codigo from inscripciones
+    );
+Commit;
+ROLLBACK ;
+
+select distinct curso_codigo from inscripciones;
+
+--3
+
+delete from cursos where profesor_id is null;
+
+--4
+delete from estudiantes where ciudad = 'Antigua';
+
+--5
+-- id: 2,22
+select * from profesores where salario < 7000;
+
+select codigo from cursos where profesor_id in (
+select id from profesores where salario < 7000
+);
+begin;
+
+delete from inscripciones where curso_codigo in (
+select codigo from cursos where profesor_id in (
+select id from profesores where salario < 7000
+) );
+
+delete from cursos where profesor_id in (
+select id from profesores where salario < 7000
+);
+
+delete from profesores where salario < 7000;
+commit;
+rollback;
+
+-- 48
+
+select * from cursos where costo>2300;
+
+select * from cursos where costo>2300;
+select * from inscripciones where curso_codigo in (
+select codigo from cursos where costo>2300
+    );
+
+begin ;
+delete from inscripciones where curso_codigo in (
+select codigo from cursos where costo>2300
+    );
+
+delete from cursos where costo>2300;
+commit ;

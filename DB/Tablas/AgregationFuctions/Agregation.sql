b7376ad83d60cb16fd15a8c0125aaa5151e6e463
JoseAv <jm.aranav@gmail.com>
2025-09-09 14:56:21 -0600

    Agregado Windows fuctions or CTES

--- DB/Tablas/AgregationFuctions/Agregation.sql
+++ DB/Tablas/AgregationFuctions/Agregation.sql
@@ -78,3 +78,68 @@ select count(*), nombre,id from cursos
     group by nombre,id
     having count(*) >= 2;
 
+-- Grouping sets
+
+--1
+select carrera,ciudad,count(*) from estudiantes
+where ciudad is not null and carrera is not null
+group by grouping sets ((carrera), (ciudad))
+
+--2
+select date_part('year',age(fecha_contratacion)) as contration, departament, avg(salario) from profesores
+where fecha_contratacion is not null
+group by grouping sets ((contration), (departament))
+;
+
+--3
+select count(*),cursos.nombre_curso,estudiantes.nombre from inscripciones
+    join cursos
+        on cursos.codigo = inscripciones.curso_codigo
+    join estudiantes
+         on estudiantes.id = inscripciones.estudiante_id
+group by grouping sets ((cursos.nombre_curso),(estudiantes.nombre));
+
+-- WINDOWS FUNCTIONS
+
+ -- Row Number, Rank, Dense Rank
+
+--1
+with major_estudents as (
+    select  carrera,nombre,promedio,rank() over (partition by carrera order by promedio desc ) from estudiantes
+        where promedio is not null and nombre is not null
+
+)
+select * from major_estudents;
+
+--2
+with enrollments_estudents as (
+    select
+        inscripciones.fecha_inscripcion,
+        inscripciones.estudiante_id,
+        row_number() over (partition by  inscripciones.estudiante_id order by estudiante_id desc )
+        from inscripciones
+)
+select * from enrollments_estudents;
+
+--3
+with rank_professors as (
+    select
+        profesores.nombre,
+        profesores.departament,
+        profesores.salario,
+        rank() over(partition by departament order by salario desc )
+    from profesores
+)
+select * from rank_professors;
+
+--4
+with rank_estudents as (
+    select
+        nombre_curso,
+        costo,
+        dense_rank() over(order by costo desc )
+    from cursos
+)
+select * from rank_estudents;
+
+
